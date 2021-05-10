b365f543227f7831d27463ea1a86686b08761154
baji babu <bajibabu683@gmail.com>
2021-05-10 16:34:26 +0530

    commit two

--- src/main/java/com/cognizant/Main.java
+++ src/main/java/com/cognizant/Main.java
@@ -1,10 +1,14 @@
 package com.cognizant;
 
+import com.cognizant.model.Person;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hai");
+		Person p=new Person();
+		p.setName("baji");
+		System.out.println("hai "+p.getName());
 
 	}
 
---
+++ src/main/java/com/cognizant/model/Person.java
@@ -0,0 +1,18 @@
+package com.cognizant.model;
+
+public class Person {
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public Person() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+}
